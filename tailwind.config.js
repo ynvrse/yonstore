20fd0a8bad40cd4d968b4ab3b5773a3a934e4a05
ynvrse <yoooooooniverse@gmail.com>
2025-01-29 14:18:46 +0800

    feat: tailwind css v4 (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,6 +53,7 @@ module.exports = {
                 },
             },
             borderRadius: {
+                xl: 'calc(var(--radius) + 4px)',
                 lg: 'var(--radius)',
                 md: 'calc(var(--radius) - 2px)',
                 sm: 'calc(var(--radius) - 4px)',
@@ -66,10 +67,20 @@ module.exports = {
                     from: { height: 'var(--radix-accordion-content-height)' },
                     to: { height: 0 },
                 },
+                'collapsible-down': {
+                    from: { height: 0 },
+                    to: { height: 'var(--radix-collapsible-content-height)' },
+                },
+                'collapsible-up': {
+                    from: { height: 'var(--radix-collapsible-content-height)' },
+                    to: { height: 0 },
+                },
             },
             animation: {
                 'accordion-down': 'accordion-down 0.2s ease-out',
                 'accordion-up': 'accordion-up 0.2s ease-out',
+                'collapsible-down': 'collapsible-down 0.2s ease-in-out',
+                'collapsible-up': 'collapsible-up 0.2s ease-in-out',
             },
         },
     },
