2b09552ecd62de16cd297b411c5487683cb86703
ynvrse <yoooooooniverse@gmail.com>
2025-01-29 14:22:23 +0800

    setup deploy at github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-const basenameProd = '';
+const basenameProd = '/yonstore/';
 
 export default defineConfig(({ command }) => {
     const isProd = command === 'build';
